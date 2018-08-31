eacc9c51af4f65cbaec46239f91a3afc06e3c3bb
Sylvie Howton <sylviehowton@sylvies-air-3.home>
2018-08-31 12:41:22 -0700

    created the main files

---
+++ main.js
@@ -0,0 +1,34 @@
+$('#submit').on('click', function(event) {
+  //prevent default behavior of anchor # from showing up on link
+event.preventDefault();
+$('#results').html('Loading...');
+
+let url = 'https://www.reddit.com/r/' + "javascript" +'.json';
+
+let promise = fetch(url).then(function(response) {
+  let promise = response.json();
+  return promise;
+});
+
+promise.then(function(response) {
+  let html = '';
+
+  response.data.children.forEach(function(thread) {
+    html += `
+    <div style="float: left; text-align: center; margin: 0.5%; width: 15%;">
+    <img
+      src="${thread.data.thumbnail}">
+    <h4 style="margin:0;">${thread.data.title}</h4>
+    <p style="margin:0;">${thread.data.score}</p>
+    <p style="margin:0;"><i>${thread.data.author}</i></p>
+    </div>
+    `;
+  })
+
+
+$('#results').html(html);
+}, function(error) {
+  console.log('error', error);
+});
+
+});
